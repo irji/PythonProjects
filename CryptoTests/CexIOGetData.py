a531c3c8d9a98890e5c28fb090e7c386ee07319d
Георгий <oilman.g@gmail.com>
2018-05-29 10:31:42 +0300

    исправления печати

--- CryptoTests/CexIOGetData.py
+++ CryptoTests/CexIOGetData.py
@@ -57,7 +57,22 @@ def CreatePairs():
 
                 pairs.append(pair)
 
-    print("Создано пар c EUR: " + str(len(pairs)))
+    #print("Создано пар c EUR: " + str(len(pairs)))
+
+    for d in usdTikers:
+        s1 = str(d["pair"]).split(":")
+
+        for b in btcTikers:
+            if str(b["pair"]).__contains__(s1[0]):
+                s2 = str(b["pair"]).split(":")
+                if s1[0] == "BTC":
+                    pair = [str(d["pair"]), str(b["pair"]), s2[0] + ":" + s1[1]]
+                else:
+                    pair = [str(d["pair"]), str(b["pair"]), s2[1] + ":" + s1[1]]
+
+                pairs.append(pair)
+
+    print("Создано пар c USD/EUR: " + str(len(pairs)))
 
 
 def CalculateRate():
@@ -74,6 +89,9 @@ def CalculateRate():
             pr3ask = 0
             pr3bid = 0
 
+            #p=str(pr).split(",")
+            #print(str(p[0]))
+
             for t in allTikers:
                 if str(t["pair"]).__contains__(p[0]):
                     pr1ask = float(t["ask"])
@@ -85,13 +103,13 @@ def CalculateRate():
                     pr3ask = float(t["ask"])
                     pr3bid = float(t["bid"])
 
-                if (pr1ask != 0.0 and pr2ask != 0.0 and pr3bid != 0.0):
+                if pr1ask != 0.0 and pr2ask != 0.0 and pr3bid != 0.0:
                     percent = (((invest / pr1ask) * pr2ask * pr3bid - invest) / invest) * 100
 
                     #print(str.format("{0} Расчетная доходность [{1};{2};{3}] равна {4}",
                     #                 datetime.datetime.now(), p[0], p[1], p[2], format(percent, ".3f")))
 
-                    if percent > 0.6:
+                    if percent > 1.2:
                         print(str.format("{0} Расчетная доходность [{1};{2};{3}] равна {4}",
                                          datetime.datetime.now(), p[0], p[1], p[2], format(percent, ".3f")))
 
@@ -102,9 +120,17 @@ def CalculateRate():
                             txt3 = str.format("{0}_ask: {1}; {0}_bid:{2}", p[2], pr3ask, pr3bid)
 
                             with open("CexIoLog.txt", "a") as fl:
-                                fl.write(txt+txt1+txt+txt3 + "\n")
+                                fl.write(txt+txt1+txt2+txt3 + "\n")
                         except:
                             print("error print to file")
+
+                        pr1ask = 0
+                        pr1bid = 0
+                        pr2ask = 0
+                        pr2bid = 0
+                        pr3ask = 0
+                        pr3bid = 0
+
     except:
         print(str.format("{0} error", datetime.datetime.now()))
 
