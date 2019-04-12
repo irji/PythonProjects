55d2b9290f72992f58770558d59d8dc1d192a4a8
Irji <oilman.g@gmail.com>
2019-04-12 17:06:56 +0300

    Чтение таблиц. Нужно сделать распознование вида таблиц

--- WorkScripts/VIP_RP_Convert/SWT_Convert.py
+++ WorkScripts/VIP_RP_Convert/SWT_Convert.py
@@ -1,41 +1,73 @@
-import pandas
-
+import pandas as pd
+from csv import reader
 
 def main():
-    _fileIn = "Krwoj1q_BFN.inc"
-    _buffer = open(_fileIn).read()
 
-    _resFile = open(_fileIn + "converted.inc", 'w')
+    _files=['Krgo_2P_BFN.inc', 'Krwoj1q_BFN.inc', 'Krwoj2q_BFN.inc', 'Krwoj3qb_BFN.inc', 'Krwoj4q_BFN.inc', 'Krwoj5q_BFN.inc', 'Krwoj6qb_BFN_v2.inc']
+    #_files = ['Krwoj1q_BFN.inc', 'Krwoj2q_BFN.inc', 'Krwoj3qb_BFN.inc', 'Krwoj4q_BFN.inc',
+    #           'Krwoj5q_BFN.inc', 'Krwoj6qb_BFN_v2.inc']
 
-    lines = open(_fileIn, "r").readlines()
+    tbl = ""
+    _resFile = open("converted.inc", 'w')
 
-    _dates = parce_to_dates(lines)
+    for _fl in _files:
+        #_fl = "Krwoj1q_BFN.inc"
+        print("Конвертирую файл {}".format(_fl))
+        tbl = tbl + read_rp_tables(_fl)
 
-    _table = ""
-    _perfData = ""
-
-    for t1 in _dates:
-        print(_date)
-
-    _perfFile.write(_perfData)
+    _resFile.write(tbl)
 
     print("Done.")
 
 
+def read_rp_tables(_fileIn):
+    #_resFile = open(_fileIn + "_converted.inc", 'w')
+    _lines = open(_fileIn, "r").readlines()
+
+    _tables = parce_to_dates(_lines)
+
+    print("Найдено {} таблиц".format(_tables.__len__()))
+
+    _tb = "SWOF\n"
+
+    for t1 in _tables:
+        if len(t1) > 3:
+            pd1 = pd.DataFrame(t1[1:], columns=['SW', 'KRW', 'KROW', 'PCWO'], dtype=float)
+            _minVal = pd1['PCWO'].min()
+
+            pd1['PCWO'] = pd1['PCWO'] - _minVal
+            #pd1['TbEnd'] = '/'
+            _tb = _tb + pd1.to_string(index=False, header=False) + "\n/\n"
+            #print(pd1)
+    #print(_tb)
+
+    #_resFile.write(_tb)
+
+    return _tb
+
+
 
 def parce_to_dates(lines):
-    _dates = []
-    _substr = ""
+    _tables = []
+    _substr = []
 
-    for ln in lines[:-1]: #бегаем до предпоследнего элемента
-        if ln.__contains__("DATE"):
-            _dates.append(_substr)
-            _substr = ""
-        _substr = _substr + ln
-    else: #если последний
-        _dates.append(_substr)
+    for ln in lines:
+        if ln.__contains__("SWT") or ln.__contains__("SGT"):
+            if ln.startswith("!") == False:
+                if len(_substr) > 0:
+                    _tables.append(_substr)
+                    _substr = []
 
-    return _dates
+        if ln.startswith("!") == False:
+            if (ln.__contains__("SWT") == False) or (ln.__contains__("SGT") == False):
+                ln = ln.strip().replace("\t", " ", 1000).replace("\n", "", 1000)
+                ar1 = ln.split(" ")
+                if len(ar1) > 3:
+                    _substr.append(ar1)
+    else:
+        _tables.append(_substr)
+
+    return _tables
 
 
 
